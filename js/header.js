6a0d25dc679e9118b7e8a813528cd18c6e6fcce3
YarElen <ahyar0647@gmail.com>
2023-10-27 21:39:29 +0700

    Memperbarui keseluruhan dari revisian

---
+++ js/header.js
@@ -0,0 +1,34 @@
+class Header extends HTMLElement {
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        this.innerHTML = `
+        <header class="header">
+        <h1 class="title">Shoes Footwear</h1>
+        <nav class="nav-head">
+            <ul class="nav-list">
+                <li class="nav-item">
+                    <a href="index.html">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="Blogs.html">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a href="AboutUs.html">About US</a>
+                </li>
+                <li class="nav-item">
+                    <a href="contact.html">Contact US</a>
+                </li>
+                <li class="nav-item">
+                    <a href="todos.html">Todos</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+        `
+    }
+}
+
+customElements.define('header-app', Header);
